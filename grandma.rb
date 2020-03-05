3de6398155d98239956ec4db96c67e49eb42ea28
Rwanda S <rwanda.ace@gmail.com>
2020-03-05 21:12:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   
   if phrase != phrase.upcase
-    return "HUH?! SPEAL UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.upcase
